cef79cab62a3bd7d7c1b5a453c8f06fb2e358e18
Silvam2017 <msilva@aggienetwork.com>
2021-10-09 18:22:45 -0400

    Removed home page. Removed privacy page. Default to rankings. Added Lions logo.

--- Prospectus/Startup.cs
+++ Prospectus/Startup.cs
@@ -56,7 +56,7 @@ namespace Prospectus
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Player}/{action=Index}/{id?}");
             });
         }
     }
